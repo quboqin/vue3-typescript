39106f320f61c0cae29ed5a9d03f4bb89135c1d4
Qubo Qin <qubo.qin.2018@gmail.com>
2021-04-18 12:22:23 +0800

    add axois to connect the local server

---
+++ src/utils/axios.ts
@@ -0,0 +1,102 @@
+import axios from 'axios'
+
+const port = process.env.VUE_APP_PORT
+console.log(process.env.VUE_APP_BASE_URL)
+axios.defaults.baseURL = port
+  ? `${process.env.VUE_APP_BASE_URL}:${process.env.VUE_APP_PORT}`
+  : `${process.env.VUE_APP_BASE_URL}`
+axios.defaults.timeout = process.env.VUE_APP_TIMEOUT
+  ? +process.env.VUE_APP_TIMEOUT
+  : 5000
+axios.defaults.headers.post['Content-Type'] =
+  'application/x-www-form-urlencoded;charset=UTF-8'
+
+axios.interceptors.request.use()
+
+axios.interceptors.response.use()
+
+function get(path: string, params: Record<string, unknown>) {
+  return new Promise((resolve, reject) => {
+    axios
+      .get(path, {
+        params: params,
+      })
+      .then((response) => {
+        resolve(response.data)
+      })
+      .catch((error) => {
+        reject(error)
+      })
+  })
+}
+
+function post(path: string, params: Record<string, unknown>) {
+  return new Promise((resolve, reject) => {
+    axios
+      .post(path, params)
+      .then((response) => {
+        resolve(response.data)
+      })
+      .catch((error) => {
+        reject(error)
+      })
+  })
+}
+
+function put(path: string, params: Record<string, unknown>) {
+  return new Promise((resolve, reject) => {
+    axios
+      .put(path, params)
+      .then((response) => {
+        resolve(response.data)
+      })
+      .catch((error) => {
+        reject(error)
+      })
+  })
+}
+
+function del(path: string, params: Record<string, unknown>) {
+  return new Promise((resolve, reject) => {
+    axios
+      .delete(path, {
+        params: params,
+      })
+      .then((response) => {
+        resolve(response.data)
+      })
+      .catch((error) => {
+        reject(error)
+      })
+  })
+}
+
+export function request(
+  method: string,
+  path: string,
+  params: Record<string, unknown>,
+): Promise<unknown> | Promise<void> {
+  if (method === 'get') {
+    return get(path, params)
+  } else if (method === 'del') {
+    return del(path, params)
+  } else if (method === 'post') {
+    return post(path, params)
+  } else if (method === 'put') {
+    return put(path, params)
+  } else {
+    return new Promise<void>((resolve) => resolve())
+  }
+}
+
+export function result(
+  method: string,
+  path: string,
+  params: Record<string, unknown>,
+  mockData?: boolean,
+): Promise<unknown> {
+  return new Promise((resolve) => {
+    if (mockData) return resolve(mockData)
+    return resolve(request(method, path, params))
+  })
+}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,8 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <div><button v-on:click="onGetUserById">Get user by id</button></div>
+    <div><button v-on:click="onGetUsers">Get users</button></div>
   </div>
 </template>
 
@@ -9,10 +11,28 @@
 import { Options, Vue } from 'vue-class-component'
 import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
+import { result } from '@/utils/axios'
+
+const getUserById = (params: Record<string, unknown> = {}) => {
+  return result('get', '/users', params)
+}
+
+type User = Record<string, unknown>
+
 @Options({
   components: {
     HelloWorld,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  user: User = { id: 'bff28903-042e-47c2-b9ee-07c3954989ec' }
+
+  async onGetUserById(): Promise<unknown> {
+    return await getUserById(this.user)
+  }
+
+  async onGetUsers(): Promise<unknown> {
+    return await getUserById()
+  }
+}
 </script>
