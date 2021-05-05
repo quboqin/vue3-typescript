33f3ceb2ba2020269c2aca0278d692e65b5b05d4
Qubo Qin <qubo.qin.2018@gmail.com>
2021-05-05 09:40:17 +0800

    install aws-amplify and aws-amplify-vue, and set "allowJs" to true in tsconfig.json

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import Amplify from 'aws-amplify'
+import awsconfig from './aws-exports'
+Amplify.configure(awsconfig)
+
 createApp(App).use(router).mount('#app')
