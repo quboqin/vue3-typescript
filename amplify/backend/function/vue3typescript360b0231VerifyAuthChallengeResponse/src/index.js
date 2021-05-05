70081a72e76e19898a4a519444a1adf5399d1bfc
Qubo Qin <qubo.qin.2018@gmail.com>
2021-05-05 08:36:24 +0800

    replace code in auth challenges

--- amplify/backend/function/vue3typescript360b0231VerifyAuthChallengeResponse/src/index.js
+++ amplify/backend/function/vue3typescript360b0231VerifyAuthChallengeResponse/src/index.js
@@ -1,13 +1,13 @@
-/*
-  this file will loop through all js modules which are uploaded to the lambda resource,
-  provided that the file names (without extension) are included in the "MODULES" env variable.
-  "MODULES" is a comma-delimmited string.
-*/
+exports.handler = async (event) => {
+  console.log(event.request)
 
-exports.handler = (event, context, callback) => {
-  const modules = process.env.MODULES.split(',');
-  for (let i = 0; i < modules.length; i += 1) {
-    const { handler } = require(`./${modules[i]}`);
-    handler(event, context, callback);
+  const expectedAnswer =
+    event.request.privateChallengeParameters.secretLoginCode
+  if (event.request.challengeAnswer === expectedAnswer) {
+    event.response.answerCorrect = true
+  } else {
+    event.response.answerCorrect = false
   }
-};
+
+  return event
+}
