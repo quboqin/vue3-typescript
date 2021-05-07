9f101aa241479e35ad1c2a975a63e4712277e25d
Qubo Qin <qubo.qin.2018@gmail.com>
2021-05-07 15:50:57 +0800

    conflict in merge

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-<<<<<<< HEAD
 import Amplify from 'aws-amplify'
 import awsconfig from './aws-exports'
 Amplify.configure(awsconfig)
 
-createApp(App).use(router).mount('#app')
-=======
 import ElementPlus from 'element-plus'
 import 'element-plus/lib/theme-chalk/index.css'
 
@@ -17,4 +14,3 @@ const app = createApp(App)
 app.use(ElementPlus)
 app.use(router)
 app.mount('#app')
->>>>>>> provide-inject
