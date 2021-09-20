60104fc40f44d9f9ff31d4e2ed0aef7e5112b290
Qubo Qin <qubo.qin.2018@gmail.com>
2021-09-20 17:47:15 +0800

    splitChunks

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,49 @@
 // vue.config.js
 const path = require('path')
 module.exports = {
-  configureWebpack: {
-    resolve: {
+  configureWebpack: (config) => {
+    if (
+      process.env.NODE_ENV === 'production' ||
+      process.env.NODE_ENV === 'staging'
+    ) {
+      // 为生产环境修改配置...
+      config.mode = 'production'
+      // 将每个依赖包打包成单独的js文件
+      const optimization = {
+        runtimeChunk: 'single',
+        splitChunks: {
+          chunks: 'all',
+          maxInitialRequests: Infinity,
+          minSize: 20000, // 依赖包超过20000bit将被单独打包
+          cacheGroups: {
+            vendor: {
+              test: /[\\/]node_modules[\\/]/,
+              name(module) {
+                // get the name. E.g. node_modules/packageName/not/this/part.js
+                // or node_modules/packageName
+                const packageName = module.context.match(
+                  /[\\/]node_modules[\\/](.*?)([\\/]|$)/,
+                )[1]
+                // npm package names are URL-safe, but some servers don't like @ symbols
+                return `npm.${packageName.replace('@', '')}`
+              },
+            },
+          },
+        },
+      }
+      Object.assign(config, {
+        optimization,
+      })
+    } else {
+      // 为开发环境修改配置...
+      config.mode = 'development'
+    }
+    Object.assign(config.resolve, {
       alias: {
         '@': path.join(__dirname, 'src'),
         '#': path.join(__dirname, 'src/views'),
       },
-    },
+    })
   },
   chainWebpack: (config) => {
     config.module
